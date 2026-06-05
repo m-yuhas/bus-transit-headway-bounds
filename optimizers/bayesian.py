ca7bc6939085830a4c3d2c1420257217b46c3456
Michael Yuhas <Michael Yuhas>
2026-06-05 16:57:06 -0500

    Bayesian Optimization Now Working

--- optimizers/bayesian.py
+++ optimizers/bayesian.py
@@ -80,46 +80,46 @@ class BayesianOptimizer():
 
     def _get_expected_improvement(self, x_new, gpr_idx):
         # First get estimate from Gaussian surrogate
-        mean_y_new, sigma_y_new = self.gauss_prs[gpr_idx].predict(np.array([x_new]), return_std=True)
-        sigma_y_new = sigma_y_new.reshape(-1,1)
-        #print(mean_y_new)
-        #print(sigma_y_new)
+        mean_y_new, sigma_y_new = self.gauss_prs[gpr_idx].predict(x_new.reshape(1,-1), return_std=True)
+        #sigma_y_new = sigma_y_new.reshape(-1,1)
         if sigma_y_new == 0.0:
             return 0.0
         #mean_c_new, sigma_c_new = self.constraint_pr.predict(np.array([x_new]), return_std=True)
         #pf = norm.cdf(self.constraint, loc=mean_c_new, scale=sigma_c_new)
         
         
-        mean_y = self.gauss_pr.predict(self.x_init)
-        max_mean_y = np.max(mean_y)
+        #mean_y = self.gauss_pr.predict(self.x_init)
+        #max_mean_y = numpy.max(mean_y)
+        mean_y = sum(self.y_prev)/len(self.y_prev)
+        max_mean_y = max(self.y_prev)
         z = (mean_y_new - max_mean_y) / sigma_y_new
         exp_imp = (mean_y_new - max_mean_y) * norm.cdf(z) + sigma_y_new * norm.pdf(z)
         return exp_imp
         #return pf * exp_imp
 
     def _get_probability_improvement(self, x_new, gpr_idx):
-        mean_y_new, sigma_y_new = self.gauss_prs[gpr_idx].predict(np.array([x_new]), return_std=True)
+        mean_y_new, sigma_y_new = self.gauss_prs[gpr_idx].predict(numpy.array([x_new]), return_std=True)
         sigma_y_new = sigma_y_new.reshape(-1, 1)
         if sigma_y_new == 0.0:
             return 0.0
         
         mean_y = self.gauss_pr.predict(self.x_init)
-        max_mean_y = np.max(mean_y)
+        max_mean_y = numpy.max(mean_y)
         pi = (mean_y_new - max_mean_y) / sigma_y_new
 
 
-        mean_c_new, sigma_c_new = self.constraint_pr.predict(np.array([x_new]), return_std=True)
+        mean_c_new, sigma_c_new = self.constraint_pr.predict(numpy.array([x_new]), return_std=True)
         pf = norm.cdf(self.constraint, loc=mean_c_new, scale=sigma_c_new)
         return pi
         #return pf * pi
 
     def _get_upper_confidence_bound(self, x_new, gpr_idx):
-        mean_y_new, sigma_y_new = self.gauss_prs[gpr_idx].predict(np.array([x_new]), return_std=True)
+        mean_y_new, sigma_y_new = self.gauss_prs[gpr_idx].predict(numpy.array([x_new]), return_std=True)
         sigma_y_new = sigma_y_new.reshape(-1, 1)
         if sigma_y_new == 0.0:
             return 0.0
 
-        mean_c_new, sigma_c_new = self.constraint_pr.predict(np.array([x_new]), return_std=True)
+        mean_c_new, sigma_c_new = self.constraint_pr.predict(numpy.array([x_new]), return_std=True)
         pf = norm.cdf(self.constraint, loc=mean_c_new, scale=sigma_c_new)
         return mean_y_new + sigma_y_new
         #return pf * (mean_y_new + sigma_y_new)  
@@ -140,11 +140,11 @@ class BayesianOptimizer():
         self.c_prev = []
         for n in range(self.n_init):
             x = {}
-            for var, support in self.variables:
+            for var, support in self.variables.items():
                 if isinstance(support, tuple):
-                    x[var] = random.random(*support)
+                    x[var] = random.uniform(*support)
                 elif isinstance(support, set):
-                    x[var] = random.choice(support)
+                    x[var] = random.choice(list(support))
             self.x_prev.append(x)
             self.y_prev.append(self.objective_f(**x))
             if self.constraints is not None:
@@ -154,8 +154,7 @@ class BayesianOptimizer():
     def _get_next_probable_point(self, x, y, grp_id):
         min_ei = float(sys.maxsize)
         x_optimal = None
-        scale = 1
-        for x_start in (np.random.random((self.batch_size, x.shape[1])) * scale):
+        for x_start in [numpy.random.uniform(-1, 1, x.shape[1]) for _ in range(self.batch_size)]:
             #response = minimize(fun=self._acquisition_function, x0 =x_start, method='L-BFGS-B')
             response = minimize(fun=self._acquisition_function, x0=x_start, args=(grp_id,), method='CG')
             if response.fun < min_ei:
@@ -163,25 +162,18 @@ class BayesianOptimizer():
                 x_optimal = response.x
         return x_optimal, min_ei
 
-    def _extend_prior_with_posterior_data(self, x, y, c):
-        self.x_init = np.append(self.x_init, x, axis=0)
-        self.y_init = np.append(self.y_init, np.array(y), axis=0)
-        self.c_init = np.append(self.c_init, np.array(c), axis=0)
-
-
     def _hist_to_numpy(self) -> tuple[list[numpy.array], list[numpy.array], list[numpy.array]]:
         x = []
         y = []
         for cat in itertools.product(*[v for v in self.categorical_vars.values()]):
             x_cat = []
             y_cat = []
-            for prevx, prevy, prevc in zip(self.x_prev, self.y_prev):
+            for prevx, prevy in zip(self.x_prev, self.y_prev):
                 if all([cat[i] == prevx[c] for i, c in enumerate(self.categorical_vars.keys())]):
                     x_cat_n = []
-                    y_cat_n = []
+                    y_cat_n = prevy
                     for idx, var in enumerate(self.ordinal_vars.keys()):
                         x_cat_n.append(prevx[var])
-                        y_cat_n.append(prevy)
                     x_cat.append(x_cat_n)
                     y_cat.append(y_cat_n)
             x.append(numpy.array(x_cat))
@@ -190,27 +182,30 @@ class BayesianOptimizer():
         # TODO: Handle constraints
         return x, y, c
 
-    def _x_next_to_dict(x_next, cat):
+    def _x_next_to_dict(self, x_next, cat):
+        print(x_next)
+        print(cat)
         cat_vals = list(itertools.product(*[v for v in self.categorical_vars.values()]))[cat]
         x = {}
-        for idx, var in self.categorical_vars.keys():
+        for idx, var in enumerate(self.categorical_vars.keys()):
             x[var] = cat_vals[idx]
-        for idx, item in self.ordinal_vars.items():
+        for idx, item in enumerate(self.ordinal_vars.items()):
             k, v = item
-            x[k] = min(max(x[idx], k[0]), k[1])
+            x[k] = min(max(x_next[idx], v[0]), v[1])
         return x
 
-    def _get_random_x(cat):
+    def _get_random_x(self, cat):
         cat_vals = list(itertools.product(*[v for v in self.categorical_vars.values()]))[cat]
         x = {}
-        for idx, var in self.categorical_vars.keys():
+        for idx, var in enumerate(self.categorical_vars.keys()):
             x[var] = cat_vals[idx]
         for var, support in self.ordinal_vars.items():
-            x[var] = random.random(*support)
+            x[var] = random.uniform(*support)
         return x
 
     def optimize(self):
         self._initialize()
+        override_stopping = False
         y_max = max(self.y_prev)
         y_max_ind = self.y_prev.index(y_max)
         optimal_x = self.x_prev[y_max_ind]
@@ -227,18 +222,19 @@ class BayesianOptimizer():
                 if len(x_cat) > 0:
                     x_scaler = MinMaxScaler((-1, 1))
                     x_scaler.fit(x_cat)
-                    y_scaler = MinMaxScaler((-1, 1))
-                    y_scaler.fit(y_cat)
-                    self.gauss_prs[cat].fit(x_scaler.transform(self.x_cat), y_scaler.transform(self.y_cat))
+                    #y_scaler = MinMaxScaler((-1, 1))
+                    #y_scaler.fit(y_cat)
+                    self.gauss_prs[cat].fit(x_scaler.transform(x_cat), y_cat.reshape(-1, 1))
                     #self.constraint_pr.fit(scaler.transform(self.x_init), self.c_init <= self.constraint)
-                    x_next, ei = self._get_next_probable_point(x_scaler.transform(self.x_cat), y_scaler.transform(self.y_cat), cat)
-                    x_next = scaler.inverse_transform(x_next.reshape(1, -1))
+                    x_next, ei = self._get_next_probable_point(x_scaler.transform(x_cat), y_cat.reshape(-1, 1), cat)
+                    x_next = x_scaler.inverse_transform(x_next.reshape(1, -1))[0]
                     x_nxts.append(self._x_next_to_dict(x_next, cat))
                     ucbs.append(ei + math.sqrt(2 * math.log(len(x_cat)) / len(self.x_prev)))
                 else:
                     x_nxts.append(self._get_random_x(cat))
                     ucbs.append(float('inf'))
             # Choose bandit arm to play
+            override_stopping = True if max(ucbs) == float('inf') else False
             nxt_idx = ucbs.index(max(ucbs))
             self.x_prev.append(x_nxts[nxt_idx])
             self.y_prev.append(self.objective_f(**x_nxts[nxt_idx]))
@@ -247,12 +243,12 @@ class BayesianOptimizer():
             print(f'Current y: {y_max}')
             
             if self.y_prev[-1] > y_max: #and c_next <= self.constraint:
-                y_max = y_prev[-1]
-                optimal_x = x_prev[-1]
+                y_max = self.y_prev[-1]
+                optimal_x = self.x_prev[-1]
                 #optimal_ei = ei
                 n_iter_no_improvement = 0
 
-            if n_iter_no_improvement > self.early_stopping and self.early_stopping > 0:
+            if n_iter_no_improvement > self.early_stopping and self.early_stopping > 0 and not override_stopping:
                 break
 
 
